16cdd3ea32f63f3357c8e6eb4c49e9e5b1b34a99
John Ingersoll <John.Ingersoll@gmail.com>
2024-02-17 10:58:32 -0500

    Added Park mode for swerve drive train

--- src/main/java/frc/robot/subsystems/drive/ExampleSwerveDriveTrain.java
+++ src/main/java/frc/robot/subsystems/drive/ExampleSwerveDriveTrain.java
@@ -10,6 +10,7 @@ import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.AnalogGyro;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.Calibration;
@@ -124,6 +125,13 @@ public class ExampleSwerveDriveTrain extends DriveSubsystem {
     m_frontRight.setDesiredState(swerveModuleStates[1]);
     m_backLeft.setDesiredState(swerveModuleStates[2]);
     m_backRight.setDesiredState(swerveModuleStates[3]);
+  }
+
+  public void driveParkControl(){
+    m_frontLeft.setDesiredState(new SwerveModuleState(0.0, Rotation2d.fromDegrees(-45.0)));
+    m_frontRight.setDesiredState(new SwerveModuleState(0.0, Rotation2d.fromDegrees(45.0)));
+    m_backLeft.setDesiredState(new SwerveModuleState(0.0, Rotation2d.fromDegrees(-45.0)));
+    m_backRight.setDesiredState(new SwerveModuleState(0.0, Rotation2d.fromDegrees(45.0)));
   }
 
   @Override
