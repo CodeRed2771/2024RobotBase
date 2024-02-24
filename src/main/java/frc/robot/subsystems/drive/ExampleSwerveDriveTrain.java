320959ad59d0f60a583e2a495b34b5ef4642d185
John Ingersoll <John.Ingersoll@gmail.com>
2024-02-24 11:02:24 -0500

    Added Fixed directional drive control

--- src/main/java/frc/robot/subsystems/drive/ExampleSwerveDriveTrain.java
+++ src/main/java/frc/robot/subsystems/drive/ExampleSwerveDriveTrain.java
@@ -127,6 +127,19 @@ public class ExampleSwerveDriveTrain extends DriveSubsystem {
     m_backRight.setDesiredState(swerveModuleStates[3]);
   }
 
+  public void driveFixedOrientation(double xSpeed, double ySpeed)
+  {
+    Rotation2d ang = new Rotation2d(xSpeed, ySpeed);
+    Translation2d Speed = new Translation2d(xSpeed,ySpeed);
+    SwerveModuleState desiredState = new SwerveModuleState(Speed.getNorm(), ang);
+
+    m_frontLeft.setDesiredState(desiredState);
+    m_frontRight.setDesiredState(desiredState);
+    m_backLeft.setDesiredState(desiredState);
+    m_backRight.setDesiredState(desiredState);
+
+  }
+
   public void driveParkControl(){
     m_frontLeft.setDesiredState(new SwerveModuleState(0.0, Rotation2d.fromDegrees(-45.0)));
     m_frontRight.setDesiredState(new SwerveModuleState(0.0, Rotation2d.fromDegrees(45.0)));
@@ -174,6 +187,7 @@ public class ExampleSwerveDriveTrain extends DriveSubsystem {
     m_backRight.setTurnOffset(BR);
     m_backLeft.setTurnOffset(BL);
     m_frontRight.setTurnOffset(FR);
+    resetEncoders();
   }
 
   private void resetEncoders(){
