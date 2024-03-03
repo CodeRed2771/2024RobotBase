2513c5d87bcc633fb811e55fefe30dfc1911640f
John Ingersoll <John.Ingersoll@gmail.com>
2024-03-02 19:23:10 -0500

    update Target positions

--- src/main/java/frc/robot/subsystems/nav/NavSubsystem.java
+++ src/main/java/frc/robot/subsystems/nav/NavSubsystem.java
@@ -26,22 +26,22 @@ public abstract class NavSubsystem extends SubsystemBase {
   public class blueFieldPositions extends fieldPositions {
     public blueFieldPositions() {
       
-        feederStation = new Pose3d(613.05,26.46,53.38, new Rotation3d(0,0,Math.toRadians(120)));
-        speakerPose = new Pose3d(652.73,218.42,40.13,new Rotation3d(0,0,Math.toRadians(180)));
-        ampPose = new Pose3d(578.77,323.00,41.38,new Rotation3d(0,0,Math.toRadians(270)));
-        trapPose[1] = new Pose3d(468.69,146.19,43.00,new Rotation3d(0,0,Math.toRadians(300)));
-        trapPose[2] = new Pose3d(468.69,177.10,43.00,new Rotation3d(0,0,Math.toRadians(60)));
-        trapPose[3] = new Pose3d(441.74,161.62,43.00,new Rotation3d(0,0,Math.toRadians(180)));
+        feederStation = new Pose3d(615.45,22.25,53.38, new Rotation3d(0,0,Math.toRadians(120)));
+        speakerPose = new Pose3d(-1.5,218.42,80.00,new Rotation3d(0,0,Math.toRadians(180)));
+        ampPose = new Pose3d(72.50,323.00,41.38,new Rotation3d(0,0,Math.toRadians(270)));
+        trapPose[1] = new Pose3d(209.48,161.62,43.00,new Rotation3d(0,0,Math.toRadians(300)));
+        trapPose[2] = new Pose3d(182.73,177.10,43.00,new Rotation3d(0,0,Math.toRadians(60)));
+        trapPose[3] = new Pose3d(182.73,146.19,43.00,new Rotation3d(0,0,Math.toRadians(180)));
     }
   }
   public class redFieldPositions extends fieldPositions {
     public redFieldPositions() {
       ampPose = new Pose3d(72.5,0,0,new Rotation3d(0,0,Math.toRadians(90)));
-      speakerPose = new Pose3d(0,110,0,new Rotation3d(0,0,0));
+      speakerPose = new Pose3d(0,105,80,new Rotation3d(0,0,0));
       trapPose[1] = new Pose3d(209.48,161.62,43.00,new Rotation3d(0,0,0));
       trapPose[2] = new Pose3d(182.73,177.10,43.00,new Rotation3d(0,0,Math.toRadians(120)));
       trapPose[3] = new Pose3d(182.73,146.19,43.00,new Rotation3d(0,0,Math.toRadians(240)));
-      feederStation = new Pose3d(38.17,26.46,53.38,new Rotation3d(0,0,Math.toRadians(60)));
+      feederStation = new Pose3d(615.00,218.5,53.38,new Rotation3d(0,0,Math.toRadians(60)));
       // speakerAngledPose[1] = new Pose3d(34.50,218.42,0,new Rotation3d(0,0,30));
       // speakerAngledPose[2] = new Pose3d(34.50,218.42,0,new Rotation3d(0,0,-30));
     }
