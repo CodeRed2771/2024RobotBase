cf347318dd15dff65bfa925878f334e923287c94
John Ingersoll <John.Ingersoll@gmail.com>
2024-03-08 17:49:34 -0500

    Added Tuneable classes for PID Gains and Slew Rate

---
+++ src/main/java/frc/robot/libs/TuneableSlewRateLimiter.java
@@ -0,0 +1,41 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.libs;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class TuneableSlewRateLimiter extends DynamicSlewRateLimiter {
+    protected String prefix;
+
+    public TuneableSlewRateLimiter(String prefix, double positiveRateLimit, double negativeRateLimit, double initialValue) {
+        super(positiveRateLimit,negativeRateLimit,initialValue);
+        this.prefix = prefix;
+    }
+
+    public TuneableSlewRateLimiter(String prefix, double rateLimit) {
+        super(rateLimit);
+        this.prefix = prefix;
+    }
+
+    public void postTuneParams() {
+        SmartDashboard.putNumber(prefix + " +rate", this.m_positiveRateLimit);
+        SmartDashboard.putNumber(prefix + " -rate", this.m_negativeRateLimit);
+    }
+
+    public void handleTuneParams() {
+
+        double posRate = SmartDashboard.getNumber(prefix + " +rate", this.m_positiveRateLimit);
+        if ((posRate != this.m_positiveRateLimit)) {
+            setPositiveRate(posRate);
+        }
+
+        double negLimit =  SmartDashboard.getNumber(prefix + " -rate", this.m_negativeRateLimit);
+        if ((negLimit != this.m_negativeRateLimit)) {
+            setNegativeRate(negLimit);
+        }
+
+    }
+
+}
