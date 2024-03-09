b211076056644c3fdf438ad9e1403e187eed51ce
John Ingersoll <John.Ingersoll@gmail.com>
2024-03-08 19:01:59 -0500

    Added symetric option to TuneableSlewRateLimiter

--- src/main/java/frc/robot/libs/TuneableSlewRateLimiter.java
+++ src/main/java/frc/robot/libs/TuneableSlewRateLimiter.java
@@ -8,6 +8,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class TuneableSlewRateLimiter extends DynamicSlewRateLimiter {
     protected String prefix;
+    protected boolean symetric = false;
 
     public TuneableSlewRateLimiter(String prefix, double positiveRateLimit, double negativeRateLimit, double initialValue) {
         super(positiveRateLimit,negativeRateLimit,initialValue);
@@ -17,25 +18,35 @@ public class TuneableSlewRateLimiter extends DynamicSlewRateLimiter {
     public TuneableSlewRateLimiter(String prefix, double rateLimit) {
         super(rateLimit);
         this.prefix = prefix;
+        symetric =true;
     }
 
     public void postTuneParams() {
-        SmartDashboard.putNumber(prefix + " +rate", this.m_positiveRateLimit);
-        SmartDashboard.putNumber(prefix + " -rate", this.m_negativeRateLimit);
+        if(symetric){
+            SmartDashboard.putNumber(prefix + " rate", this.m_positiveRateLimit);
+        } else {
+            SmartDashboard.putNumber(prefix + " +rate", this.m_positiveRateLimit);
+            SmartDashboard.putNumber(prefix + " -rate", this.m_negativeRateLimit);
+        }
     }
 
     public void handleTuneParams() {
+        if(symetric){
+            double posRate = SmartDashboard.getNumber(prefix + " rate", this.m_positiveRateLimit);
+            if ((posRate != this.m_positiveRateLimit)) {
+                setPositiveRate(posRate);
+                setNegativeRate(-posRate);
+            }
+        } else {
+            double posRate = SmartDashboard.getNumber(prefix + " +rate", this.m_positiveRateLimit);
+            if ((posRate != this.m_positiveRateLimit)) {
+                setPositiveRate(posRate);
+            }
 
-        double posRate = SmartDashboard.getNumber(prefix + " +rate", this.m_positiveRateLimit);
-        if ((posRate != this.m_positiveRateLimit)) {
-            setPositiveRate(posRate);
+            double negLimit =  SmartDashboard.getNumber(prefix + " -rate", this.m_negativeRateLimit);
+            if ((negLimit != this.m_negativeRateLimit)) {
+                setNegativeRate(negLimit);
+            }
         }
-
-        double negLimit =  SmartDashboard.getNumber(prefix + " -rate", this.m_negativeRateLimit);
-        if ((negLimit != this.m_negativeRateLimit)) {
-            setNegativeRate(negLimit);
-        }
-
     }
-
 }
