a6d812d3c03b2d728d7f24852dbeaa8df98591a1
John Ingersoll <john.ingersoll@ge.com>
2024-03-27 21:48:17 -0400

    Added velocity to Nav

--- src/main/java/frc/robot/subsystems/nav/PracticeRobotNav.java
+++ src/main/java/frc/robot/subsystems/nav/PracticeRobotNav.java
@@ -13,6 +13,7 @@ import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform2d;
+import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
@@ -106,6 +107,10 @@ public class PracticeRobotNav extends NavSubsystem {
     @Override
     public Pose2d getPoseInField() {
         return poseEstimator.getEstimatedPosition();
+    }
+
+    public Translation2d getVelInRobot(){
+        return gyro.getVelocity3d().toTranslation2d();
     }
 
     private void updateTestPoint(String prefix, Pose2d pose) {
